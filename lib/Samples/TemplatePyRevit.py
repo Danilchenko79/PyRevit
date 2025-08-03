1e17de815ddf2f2a91f76e682cd8d940d9dcc130
Danilchenko79 <dima20111999@gmail.com>
2025-08-03 19:07:55 +0300

    1

--- lib/Samples/TemplatePyRevit.py
+++ lib/Samples/TemplatePyRevit.py
@@ -1,30 +1,14 @@
 # -*- coding: utf-8 -*-
 __title__ = "EF Template"                           # Name of the button displayed in Revit UI
-__doc__ = """Version = 1.0
-Date    = 20.04.2022
-_____________________________________________________________________
-Description:
-This is a template file for pyRevit Scripts.
-_____________________________________________________________________
-How-to:
--> Click on the button
--> Change Settings(optional)
--> Make a change
-_____________________________________________________________________
-Last update:
-- [24.04.2022] - 1.0 RELEASE
-_____________________________________________________________________
-To-Do:
-- 
-_____________________________________________________________________
-Author: Erik Frits"""                                           # Button Description shown in Revit UI
+__doc__ = """
+
+
+"""
+
 
 # EXTRA: You can remove them.
-__author__ = "Erik Frits"                                       # Script's Author
-__helpurl__ = "https://www.youtube.com/watch?v=YhL_iOKH-1M"     # Link that can be opened with F1 when hovered over the tool in Revit UI.
-# __highlight__ = "new"                                           # Button will have an orange dot + Description in Revit UI
-__min_revit_ver__ = 2019                                        # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
-__max_revit_ver = 2022                                          # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
+__author__ = "Dima D"                                       # Script's Author
+                                      # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
 # __context__     = ['Walls', 'Floors', 'Roofs']                # Make your button available only when certain categories are selected. Or Revit/View Types.
 
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
@@ -35,6 +19,11 @@ __max_revit_ver = 2022                                          # Limit your Scr
 import os, sys, math, datetime, time                                    # Regular Imports
 from Autodesk.Revit.DB import *                                         # Import everything from DB (Very good for beginners)
 from Autodesk.Revit.DB import Transaction, FilteredElementCollector     # or Import only classes that are used.
+from Autodesk.Revit.UI.Selection import Selection
+from Autodesk.Revit.UI.Selection import ObjectType
+
+
+
 
 # pyRevit
 from pyrevit import revit, forms                                        # import pyRevit modules. (Lots of useful features)
@@ -53,8 +42,8 @@ from System.Collections.Generic import List # List<ElementType>() <- it's specia
 # ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
 #  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ 📦 VARIABLES
 # ==================================================
-doc   = __revit__.ActiveUIDocument.Document   # Document   class from RevitAPI that represents project. Used to Create, Delete, Modify and Query elements from the project.
-uidoc = __revit__.ActiveUIDocument          # UIDocument class from RevitAPI that represents Revit project opened in the Revit UI.
+doc   = __revit__.ActiveUIDocument.Document   #type:Document
+uidoc = __revit__.ActiveUIDocument            #type:UIDocument
 app   = __revit__.Application                 # Represents the Autodesk Revit Application, providing access to documents, options and other application wide data and settings.
 PATH_SCRIPT = os.path.dirname(__file__)     # Absolute path to the folder where script is placed.
 
@@ -82,23 +71,14 @@ PATH_SCRIPT = os.path.dirname(__file__)     # Absolute path to the folder where 
 # ==================================================
 if __name__ == '__main__':
 
-    # START CODE HERE
-
-    # Use Transaction for Changes.
     t = Transaction(doc,__title__)  # Transactions are context-like objects that guard any changes made to a Revit model.
-    # AVOID  placing Transaction inside of your loops! It will drastically reduce perfomance of your script.
 
-    # You need to use t.Start() and t.Commit() to make changes to a Project.
     t.Start()  # <- Transaction Start
 
-    #- CHANGES TO REVIT PROJECT HERE
 
     t.Commit()  # <- Transaction End
 
 
-    # Notify user that script is complete.
-    print('-' * 50)
-    print('Hold ALT + Click on the button to open its source folder.')
-    print('Adjust this tmeplate to your needs and duplicate to new buttons.')
+selection = uidoc.Selection #type:Selection
 
-    print('Template has been developed by Erik Frits.')
+sel= selection.PickObject(ObjectType.Element)
